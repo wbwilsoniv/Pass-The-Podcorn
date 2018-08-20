1469e11e53d106ebc337f72f8cb51031632df50d
Shua Wolff <shtawo11@gmail.com>
2018-08-20 12:09:28 -0400

    Added the schema for db

---
+++ app/db/schema.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS podcasts;
+DROP TABLE IF EXISTS reviews;
+
+CREATE TABLE podcasts (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    creator VARCHAR(255) NOT NULL,
+    episodes INTEGER NOT NULL,
+    description TEXT,
+    poster_url VARCHAR(255),
+    trailer_url VARCHAR(255)
+);
+
+CREATE TABLE reviews (
+    review_id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    podcast_id INTEGER NOT NULL REFERENCES podcasts(id),
+    upvote INTEGER DEFAULT 0,
+    downvote INTEGER DEFAULT 0
+)

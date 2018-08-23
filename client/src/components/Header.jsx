1f8dc761d56cad6ff2f71075e61146d1c141ddb7
Will W <38361826+wbwilsoniv@users.noreply.github.com>
2018-08-23 15:55:51 -0400

    logo and favicon (#39)
    * Styling changes.
    
    * merged files, resolved thumbnail issue

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,6 +6,7 @@ function Header(props) {
             <h2 className="title pass">Pass</h2>
             <h2 className="title the">the<br/></h2>
             <h2 className="title">Podcorn<br/><br/></h2>
+            <img src="https://user-images.githubusercontent.com/38361826/44546122-fcde9500-a6e4-11e8-804d-dad420704178.png" alt="Pass the Podcorn Logo" id="ptpLogo"/>
         </div>
     )
 }

6f4082a08019cd714e35341898c6ee6b11be8959
Shua <30534934+shuawolff@users.noreply.github.com>
2018-08-24 09:39:37 -0400

    Updated font (#49)
    * Updated font

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3001';
+const BASE_URL = 'http://localhost:3001';
 
 export function fetchPodcasts() {
   return fetch(`${BASE_URL}/podcasts`)
